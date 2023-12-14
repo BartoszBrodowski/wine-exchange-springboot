e77b6b48823f8e411d5b09faf955795fde6b0572
BartoszBrodowski <bartolini10058@gmail.com>
2023-12-14 14:08:02 +0100

    feat(oauth): google oauth

---
+++ api/src/main/java/com/wineexchange/api/Controllers/OAuthController.java
@@ -0,0 +1,16 @@
+package com.wineexchange.api.Controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OAuthController {
+
+    @GetMapping("/login/success")
+    public String loginSuccess(@AuthenticationPrincipal Authentication authentication) {
+        return "redirect:/home";
+    }
+}
+
